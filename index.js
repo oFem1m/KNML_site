95acfdedc7428b74806e4dec905e785215aa3e4a
alex <volohovaleksandr1974@gmail.com>
2023-07-16 16:35:58 +0300

    header was added

--- index.js
+++ index.js
@@ -1,60 +1,84 @@
 const model = [
-    {type: 'title', value: "Hello, world"},
+    {type: 'title', value: "Kotlin Numerical Methods Library", options: {
+        tag: 'h1',
+        styles: {
+            color: '#fff',
+            'text-align': 'center',
+        }
+        }},
     {type: 'text', value: "It`s my first site!"},
     {type: 'columns', value: [
         '1', '2', '3'
         ]},
-    {type: 'image', value: './assets/kotlin.jpg'}
+    {type: 'image', value: './assets/kotlin.jpg', options: {
+        styles: {
+            maxWidth: '225px', // Set the maximum width of the image
+            maxHeight: '225px', // Set the maximum height of the image
+        }
+        }}
 ]
 
 const $site = document.querySelector('#site')
+const $head = document.querySelector('#head')
 
 model.forEach(block =>{
     let html = ''
     if (block.type === 'title'){
         html = title(block)
-    } else if (block.type === 'text'){
+    } else if (block.type === 'image'){
+        html = image(block, css(block.options.styles))
+    }
+    $head.insertAdjacentHTML('beforeend', html)
+})
+
+/*
+model.forEach(block =>{
+    let html = ''
+    if (block.type === 'text'){
         html = text(block)
     } else if (block.type === 'columns'){
         html = columns(block)
-    } else if (block.type === 'image'){
-        html = image(block)
     }
-
     $site.insertAdjacentHTML('beforeend', html)
 })
 
+ */
 
+
+//content
 function title(block){
-    return `
-    <div class="row">
-        <div class="col-sm">
-            <p>${block.value}</p>
-        </div>
-    </div>
-    `
+    const {tag = 'h1', styles} = block.options
+    return row(col(`<${tag}>${block.value}</${tag}>`), css(styles))
 }
 function text(block){
-    return `
-    <div class="row">
-        <div class="col-sm">
-            <p>${block.value}</p>
-        </div>
-    </div>
-    `
+    return row(col(`<p>${block.value}</p>`))
 }
 function columns(block){
-    const html = block.value.map(item => `<div class="col-sm">${item}</div>`)
-    return `
-    <div class="row">
-        ${html.join('')}
-    </div>
-    `
+    const html = block.value.map(col).join('')
+    return row(html)
 }
-function image(block){
-    return `
-    <div class="row">
-        <img src="${block.value}" alt=""/>
-    </div>
-    `
+
+function image(block, styles=''){
+    return row(`<img style="${styles}" src="${block.value}" alt=""/>`)
+}
+
+
+//utils
+function row(content, styles=''){
+    return `<div class="row" style="${styles}">
+                ${content}
+            </div>`
+}
+function col(content){
+    return `<div class="col-sm">
+                ${content}
+            </div>`
+}
+
+function css(styles = {}) {
+    const keys = Object.keys(styles)
+    const array = keys.map(key => {
+        return `${key} : ${styles[key]}`
+    })
+    return array.join(';')
 }
